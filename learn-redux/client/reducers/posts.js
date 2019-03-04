09afe9016162a43ce5d9e0baa5889c2f5601b1a5
Herve Desrosiers <hdesrosiers@mtlt30rjg5l-mac.corp.stingraydigital.com>
2019-03-04 07:42:44 -0500

    video 11 displaying redux state inside components

--- learn-redux/client/reducers/posts.js
+++ learn-redux/client/reducers/posts.js
@@ -3,7 +3,8 @@
 //1. the action (info about what happened)
 //2. copy of current state
 
-function posts(action, state = []) {
+function posts(state = [], action) {
+  console.log("The post will change");
   console.log(state, action);
   return state;
 }
