633f5604b69eb8dc2b2f2c854fdbfc18d62fab46
Herve Desrosiers <hdesrosiers@mtlt30rjg5l-mac.corp.stingraydigital.com>
2019-03-07 07:05:29 -0500

    redux tools

--- learn-redux/client/reduxstagram.js
+++ learn-redux/client/reduxstagram.js
@@ -7,6 +7,10 @@ import PhotoGrid from "./components/PhotoGrid";
 import { Router, Route, IndexRoute, browserHistory } from "react-router";
 import { Provider } from "react-redux";
 import store, { history } from "./store";
+// import Raven from "raven-js";
+// import { sentry_url } from "./data/config";
+
+// Raven.config(sentry_url).install();
 
 const router = (
   <Provider store={store}>
--- learn-redux/client/store.js
+++ learn-redux/client/store.js
@@ -14,8 +14,25 @@ const defaultState = {
   comments
 };
 
-const store = createStore(rootReducer, defaultState);
+// const enhancers = compose(
+//   window.devToolsExtension ? window.devToolsExtension() : f => f
+// );
+
+const enhancers = compose(
+  window.__REDUX_DEVTOOLS_EXTENSION__
+    ? window.__REDUX_DEVTOOLS_EXTENSION__()
+    : f => f
+);
+
+const store = createStore(rootReducer, defaultState, enhancers);
 
 export const history = syncHistoryWithStore(browserHistory, store);
+
+if (module.hot) {
+  module.hot.accept("./reducers/", () => {
+    const nextRootReducer = require("./reducers/index").default;
+    store.replaceReducer(nextRootReducer);
+  });
+}
 
 export default store;
